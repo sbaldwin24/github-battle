cc76d9bde4936c3d721a3063c071903e7e04d0e2
Sterling Baldwin <sterling.baldwin@gmail.com>
2017-12-10 18:37:46 -0600

    fixed className and inline styles

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -96,8 +96,8 @@ class Popular extends React.Component {
 				{
 					!this.state.repos
 						? <div className="loading">
-								<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-   								<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+								<svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+   								<circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
 								</svg>
 							</div>
 						: <RepoGrid repos={this.state.repos} />
